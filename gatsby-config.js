1afd754146935fb9c86339e6ab86b41c1cb9e69d
samuelokediji <samuelolaideokediji@gmail.com>
2020-07-19 02:44:37 +0100

    gatsby don happen

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,21 +3,25 @@ module.exports = {
     {
       resolve: `gatsby-theme-blog`,
       options: {},
+      resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown-pages`,
+      path: `${__dirname}/content/posts/blog`, 
     },
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
+    title: `My Ideate Blog `,
     author: `My Name`,
     description: `My site description...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/ideatedigital`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/samuelokediji`,
       },
     ],
   },
